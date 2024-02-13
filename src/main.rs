2ea4eaae66562def0efac273246db45c8bdd12c6
LumisDev <153009693+LumisDev@users.noreply.github.com>
2024-02-13 17:44:11 +0100

    Adding PSP lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+psp::module!("amogus", 1, 0);
+fn psp_main(){
+    psp::enable_home_button();
+    psp::dprintln!("Hello from Rust PSP!");
 }
