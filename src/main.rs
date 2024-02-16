c6805320b3238299677c0ada28f7cfc744e5b290
LumisDev <153009693+LumisDev@users.noreply.github.com>
2024-02-16 16:34:38 +0100

    Add GUI and GFX

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 #![no_std]
 #![no_main]
-psp::module!("amogus", 1, 0);
+mod gfx;
+psp::module!("Among Us PSP Version", 1, 0);
 fn psp_main(){
     psp::enable_home_button();
-    psp::dprintln!("Hello from Rust PSP!");
+    unsafe {
+        let mut renderer = gfx::Renderer::new();
+        loop {
+            renderer.clear(0xFFFFCA82);
+            renderer.swap_buffers();
+            psp::sys::sceDisplayWaitVblankStart();
+        }
+    }
 }
